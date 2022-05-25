6a6626e31dd37ed01ceb68f0cd185f16f72f2068
Graham Otte <go@grahamotte.com>
2022-05-25 00:29:26 -0700

    Bump everything to latest versions and remove not needed deps

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,13 @@ import * as ActiveStorage from "@rails/activestorage";
 import DemoApp from "../pages/demo_app";
 import Rails from "@rails/ujs";
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
 
 Rails.start();
 ActiveStorage.start();
 
-document.addEventListener("DOMContentLoaded", () => {
-  render(<DemoApp />, document.body.appendChild(document.createElement("div")));
-});
+const root = ReactDOM.createRoot(
+  document.body.appendChild(document.createElement("div"))
+);
+
+root.render(<DemoApp />);
