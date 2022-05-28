cbab320231188d00c24bda4f66b966e19324b1d7
Graham Otte <go@grahamotte.com>
2022-05-27 19:52:51 -0700

    Git prefix

--- bin/lib/patches/sync_archive_code.rb
+++ bin/lib/patches/sync_archive_code.rb
@@ -4,16 +4,20 @@ module Patches
       def needed?
         return false if archive_repo.blank?
 
-        Utils.run_local("git ls-remote -h #{archive_repo} HEAD", bool: true)
+        Utils.run_local("#{git_prefix} ls-remote -h #{archive_repo} HEAD", bool: true)
       end
 
       def apply
-        Utils.run_local('git remote remove archive', bool: true)
-        Utils.run_local("git remote add archive #{archive_repo}")
-        Utils.run_local('git push -f archive master')
+        Utils.run_local("#{git_prefix} remote remove archive", bool: true)
+        Utils.run_local("#{git_prefix} remote add archive #{archive_repo}")
+        Utils.run_local("#{git_prefix} push -f archive master")
       end
 
       private
+
+      def git_prefix
+        "GIT_SSH_COMMAND='ssh -i #{Secrets.id_rsa_path}' git"
+      end
 
       def archive_repo
         return 'git@github.com:grahamotte/railsgun.com.git' if Utils.domain_name == 'railsgun.com'
